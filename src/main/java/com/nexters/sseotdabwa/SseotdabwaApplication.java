fa97662d155cce518389a12413c327c46f8006fc
parksomii <somi020605@gmail.com>
2026-01-11 22:56:53 +0900

    init: 프로젝트 초기 세팅

---
+++ src/main/java/com/nexters/sseotdabwa/SseotdabwaApplication.java
@@ -0,0 +1,13 @@
+package com.nexters.sseotdabwa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SseotdabwaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SseotdabwaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nexters/sseotdabwa/SseotdabwaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nexters.sseotdabwa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SseotdabwaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
