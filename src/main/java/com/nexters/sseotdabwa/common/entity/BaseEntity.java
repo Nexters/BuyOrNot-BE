9cc91df2d47b01bcba57e9169ad511bf9233883f
haeun1107 <haeun9394@gmail.com>
2026-01-20 00:57:40 +0900

    refactor: @EnableJpaAuditing을 JpaConfig로 분리

--- src/main/java/com/nexters/sseotdabwa/common/entity/BaseEntity.java
+++ src/main/java/com/nexters/sseotdabwa/common/entity/BaseEntity.java
@@ -1,5 +1,6 @@
 package com.nexters.sseotdabwa.common.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import java.time.LocalDateTime;
