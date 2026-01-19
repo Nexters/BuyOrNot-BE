9fcdcb214c233541a0f3390b0a6a1e756b9fa334
haeun1107 <haeun9394@gmail.com>
2026-01-20 00:53:08 +0900

    refactor: createdAt 업데이트 방지

--- src/main/java/com/nexters/sseotdabwa/common/entity/BaseEntity.java
+++ src/main/java/com/nexters/sseotdabwa/common/entity/BaseEntity.java
@@ -14,6 +14,7 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 public class BaseEntity {
 
     @CreatedDate
+    @Column(updatable = false)
     private LocalDateTime createdAt;
 
     @LastModifiedDate
