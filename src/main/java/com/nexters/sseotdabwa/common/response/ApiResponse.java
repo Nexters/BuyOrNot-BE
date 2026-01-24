9e52021d6905cd03d5ae111a2756e0da56a3f22a
haeun1107 <haeun9394@gmail.com>
2026-01-24 17:07:52 +0900

    chore: ApiResponse 응답 포맷 수정

--- src/main/java/com/nexters/sseotdabwa/common/response/ApiResponse.java
+++ src/main/java/com/nexters/sseotdabwa/common/response/ApiResponse.java
@@ -5,39 +5,48 @@ import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import org.springframework.http.HttpStatus;
 
 @Getter
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
-@JsonPropertyOrder({"data", "message", "status"})
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({"data", "message", "status", "errorCode"})
 public class ApiResponse<T> {
 
-    @JsonInclude(JsonInclude.Include.NON_NULL)
     private final T data;
-
     private final String message;
 
     /**
-     * 요구 포맷: status 필드는 문자열
-     * (SUCCESS/ERROR 등)
+     * 요구 포맷: status 필드는 문자열 "201", "404"
      */
     private final String status;
 
-    public static <T> ApiResponse<T> success(T data) {
-        return new ApiResponse<>(data, "성공입니다.", ApiStatus.SUCCESS.getValue());
-    }
-
-    public static <T> ApiResponse<T> success(T data, String message) {
-        return new ApiResponse<>(data, message, ApiStatus.SUCCESS.getValue());
-    }
-
-    public static ApiResponse<Void> success() {
-        return new ApiResponse<>(null, "성공입니다.", ApiStatus.SUCCESS.getValue());
-    }
-
     /**
-     * 에러 응답은 나중에 GlobalExceptionHandler에서 통일해서 사용
+     * 에러 응답일 때만 존재
      */
-    public static <T> ApiResponse<T> error(String message) {
-        return new ApiResponse<>(null, message, ApiStatus.ERROR.getValue());
+    private final String errorCode;
+
+    public static <T> ApiResponse<T> success(T data, HttpStatus httpStatus) {
+        return new ApiResponse<>(data, "성공입니다.", String.valueOf(httpStatus.value()), null);
+    }
+
+    public static <T> ApiResponse<T> success(T data, String message, HttpStatus httpStatus) {
+        return new ApiResponse<>(data, message, String.valueOf(httpStatus.value()), null);
+    }
+
+    public static ApiResponse<Void> success(HttpStatus httpStatus) {
+        return new ApiResponse<>(null, "성공입니다.", String.valueOf(httpStatus.value()), null);
+    }
+
+    public static ApiResponse<Void> success(String message, HttpStatus httpStatus) {
+        return new ApiResponse<>(null, message, String.valueOf(httpStatus.value()), null);
+    }
+
+    public static ApiResponse<Void> error(String message, HttpStatus httpStatus, String errorCode) {
+        return new ApiResponse<>(null, message, String.valueOf(httpStatus.value()), errorCode);
+    }
+
+    public static ApiResponse<Void> error(String message, HttpStatus httpStatus) {
+        return new ApiResponse<>(null, message, String.valueOf(httpStatus.value()), null);
     }
 }
