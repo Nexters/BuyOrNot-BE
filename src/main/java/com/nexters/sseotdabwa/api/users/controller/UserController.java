1c9fbe61dd07917318a7867663d5215c36c8386e
parksomii <somi020605@gmail.com>
2026-01-25 16:22:06 +0900

    feat: 내 정보 조회 API 구현

--- src/main/java/com/nexters/sseotdabwa/api/users/controller/UserController.java
+++ src/main/java/com/nexters/sseotdabwa/api/users/controller/UserController.java
@@ -1,11 +1,15 @@
 package com.nexters.sseotdabwa.api.users.controller;
 
+import com.nexters.sseotdabwa.api.users.dto.UserResponse;
+import com.nexters.sseotdabwa.api.users.facade.UserFacade;
+import com.nexters.sseotdabwa.common.response.ApiResponse;
+import com.nexters.sseotdabwa.common.security.CurrentUser;
+import com.nexters.sseotdabwa.domain.users.entity.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.nexters.sseotdabwa.api.users.facade.UserFacade;
-
-import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequestMapping("/api/v1/users")
@@ -13,4 +17,11 @@ import lombok.RequiredArgsConstructor;
 public class UserController implements UserControllerSpec {
 
     private final UserFacade userFacade;
+
+    @Override
+    @GetMapping("/me")
+    public ApiResponse<UserResponse> getMyInfo(@CurrentUser User user) {
+        UserResponse response = userFacade.getMyInfo(user);
+        return ApiResponse.success(response, HttpStatus.OK);
+    }
 }
--- src/main/java/com/nexters/sseotdabwa/api/users/facade/UserFacade.java
+++ src/main/java/com/nexters/sseotdabwa/api/users/facade/UserFacade.java
@@ -1,14 +1,20 @@
 package com.nexters.sseotdabwa.api.users.facade;
 
-import com.nexters.sseotdabwa.domain.users.service.UserService;
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
-import org.springframework.transaction.annotation.Transactional;
 
+import com.nexters.sseotdabwa.api.users.dto.UserResponse;
+import com.nexters.sseotdabwa.domain.users.entity.User;
+
+/**
+ * 사용자 관련 비즈니스 로직을 조합하는 Facade
+ */
 @Component
-@RequiredArgsConstructor
-@Transactional(readOnly = true)
 public class UserFacade {
 
-    private final UserService userService;
+    /**
+     * 현재 로그인한 사용자 정보 조회
+     */
+    public UserResponse getMyInfo(User user) {
+        return UserResponse.from(user);
+    }
 }
