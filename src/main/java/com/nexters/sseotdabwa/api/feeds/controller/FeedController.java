2e08e39bd9433646d507d58c1c0ac8b2912475d4
SEO HAEUN <haeun9394@gmail.com>
2026-01-31 16:30:04 +0900

    feat: Feed Controller 추가

---
+++ src/main/java/com/nexters/sseotdabwa/api/feeds/controller/FeedController.java
@@ -0,0 +1,16 @@
+package com.nexters.sseotdabwa.api.feeds.controller;
+
+import com.nexters.sseotdabwa.api.feeds.facade.FeedFacade;
+
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/feeds")
+@RequiredArgsConstructor
+public class FeedController {
+
+    private final FeedFacade feedFacade;
+}
