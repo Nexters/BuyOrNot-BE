de388691fa184e37c5666593ecb775a4e530e10f
SEO HAEUN <haeun9394@gmail.com>
2026-02-07 01:45:52 +0900

    feat: FeedImage entity, repository, service 추가

---
+++ src/main/java/com/nexters/sseotdabwa/domain/feeds/repository/FeedImageRepository.java
@@ -0,0 +1,7 @@
+package com.nexters.sseotdabwa.domain.feeds.repository;
+
+import com.nexters.sseotdabwa.domain.feeds.entity.FeedImage;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FeedImageRepository extends JpaRepository<FeedImage, Long> {
+}
