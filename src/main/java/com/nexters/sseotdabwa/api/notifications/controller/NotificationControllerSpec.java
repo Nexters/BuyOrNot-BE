1c1d9cf1e1272704fa690298f5bb8cf4c7b80515
SEO HAEUN <haeun9394@gmail.com>
2026-02-17 23:42:21 +0900

    feat: FeedController 및 Spec 인터페이스 적용

---
+++ src/main/java/com/nexters/sseotdabwa/api/notifications/controller/NotificationControllerSpec.java
@@ -0,0 +1,8 @@
+package com.nexters.sseotdabwa.api.notifications.controller;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+@Tag(name = "Notifications", description = "알림 API")
+public interface NotificationControllerSpec {
+
+}
