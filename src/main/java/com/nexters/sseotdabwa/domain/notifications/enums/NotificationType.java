40a52e0f5c9e5b9b5d1518f65f94e422698b0839
SEO HAEUN <haeun9394@gmail.com>
2026-02-17 23:41:35 +0900

    feat: Notification entity 및 enum 추가
    - MY_FEED_CLOSED, PARTICIPATED_FEED_CLOSED 타입 추가

---
+++ src/main/java/com/nexters/sseotdabwa/domain/notifications/enums/NotificationType.java
@@ -0,0 +1,6 @@
+package com.nexters.sseotdabwa.domain.notifications.enums;
+
+public enum NotificationType {
+    MY_FEED_CLOSED,            // 내가 올린 투표 종료
+    PARTICIPATED_FEED_CLOSED   // 내가 참여한 투표 종료
+}
