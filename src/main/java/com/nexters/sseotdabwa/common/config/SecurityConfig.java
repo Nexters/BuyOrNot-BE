c4c8a02146896a65b41091ae4c93b8ed748feef1
SEO HAEUN <haeun9394@gmail.com>
2026-09-08 21:47:02 +0900

    chore: CORS 허용 오리진에 웹 개발서버(dev-web.buy-or-not.com) 추가

--- src/main/java/com/nexters/sseotdabwa/common/config/SecurityConfig.java
+++ src/main/java/com/nexters/sseotdabwa/common/config/SecurityConfig.java
@@ -87,6 +87,7 @@ public class SecurityConfig {
                 "http://localhost:3000",
                 "http://localhost:5173",
                 "https://dev.buy-or-not.com",
+                "https://dev-web.buy-or-not.com",
                 "https://buy-or-not.com",
                 "https://api.buy-or-not.com"
         ));
